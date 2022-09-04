96638201531cb0f3eb9263cc2b64accb19e3e5fb
Sakura Jassen <tfd2@live.de>
2022-09-05 01:42:29 +0200

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -21,7 +21,7 @@ class MainCog(commands.Cog):
 
     @commands.command()
     async def pingTile(self, ctx):
-        await ctx.send('pong pog10')
+        await ctx.send('pong pog11')
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
